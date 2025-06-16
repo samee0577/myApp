556dbaa4c0fcf43c61171488e38cd6df420ede3b
samee <shaikhsamee0577@gmail.com>
2025-06-16 18:58:26 +0530

    minor data fetch change

--- data/fetch.js
+++ data/fetch.js
@@ -1,10 +1,9 @@
 import axios from 'axios';
 import { TMDB_ACCESS_TOKEN, BASE_URL, IMAGE_BASE_URL } from '../api';
 
-export const fetchTrendingMovies = async (): Promise<any[]> => {
+export const fetchTrendingMovies = async () => {
   try {
-    await new Promise(resolve => setTimeout(resolve, 2000)); // delibrate Delay for 2 seconds
-    
+
     const response = await axios.get(`${BASE_URL}/trending/movie/week`, {
       headers: {
         Authorization: `Bearer ${TMDB_ACCESS_TOKEN}`,
@@ -12,13 +11,13 @@ export const fetchTrendingMovies = async (): Promise<any[]> => {
     });
     console.log('Trending movies fetched successfully:', response.data.results);
     return response.data.results;
-  } catch (error: any) {
+  } catch (error) {
     console.log('Error fetching trending movies:', error.message);
     return [];
   }
 };
 
-export const getImageUrl = (path: string | null): string | null => 
+export const getImageUrl = (path) => 
   path ? `${IMAGE_BASE_URL}${path}` : null;
   
 
